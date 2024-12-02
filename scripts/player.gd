fb932d98d9461c0001c57ef0511910a025a71933
aymeric-devv <aymericsursnap@gmail.com>
2024-12-02 18:09:09 +0100

    Better movements and animations build by me

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,16 +3,16 @@ extends CharacterBody2D
 @export var max_speed : int = 100
 @export var gravity : float = 20
 @export var jump_force : int = 225
-@export var acceleration : int = 150
+@export var acceleration : int = 50
 @export var jump_buffer_time : int  = 15
-@export var cayote_time : int = 15
+@export var cayote_time : int = 7
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 var jump_buffer_counter : int = 0
 var cayote_counter : int = 0
-var requested_animation = "idle"
-var player_state = "idle"
+var player_state = "idle" #Store player state in a variable
+var idle_cooldown = 10 #A cooldown beetwen each idles animations
 
 func _physics_process(_delta):
 	if not is_on_floor():
@@ -24,38 +24,52 @@ func _physics_process(_delta):
 			
 	if Input.is_action_pressed("move_right"): #Go right
 		velocity.x += acceleration
-		if player_state != "jumping": 
-			requested_animation = "run"
+		if is_on_floor() and player_state != "jumping": #If player isn't jumping to prevent animation conflict
+			animated_sprite.play("run") 
 		animated_sprite.flip_h = false
+		idle_cooldown = 10.0 #Reset the player idle cooldown
 	elif Input.is_action_pressed("move_left"): #Go left
 		velocity.x -= acceleration
-		if player_state != "jumping":
-			requested_animation = "run"
+		if is_on_floor() and player_state != "jumping": #If player isn't jumping to prevent animation conflict
+			animated_sprite.play("run")
 		animated_sprite.flip_h = true
+		idle_cooldown = 10.0 #Reset the player idle cooldown
 	else: #No movements
 		velocity.x = lerp(velocity.x,0.0,0.2)
-		if player_state != "jumping":
-			requested_animation = "wait" #TO DO : Set idle after 2s and sleeping after 7s
+		if is_on_floor() and player_state != "jumping": #If player isn't jumping to prevent animation conflict	 	
+			player_state = "idle"
+			idle_cooldown -= 0.01
+			if idle_cooldown > 9.5:
+				animated_sprite.play("wait") 
+			elif idle_cooldown > 3:
+				animated_sprite.play("idle")
+			else:
+				animated_sprite.play("sleep")
 		
+	if not is_on_floor() and player_state != "jumping":
+		animated_sprite.play("in-air")
+
 	velocity.x = clamp(velocity.x, -max_speed, max_speed)
 	
 	if is_on_floor():
 		cayote_counter = cayote_time
 		player_state = "nothing"
 		
-	if Input.is_action_just_pressed("jump"): 
-		jump_buffer_counter = jump_buffer_time
-	
 	if jump_buffer_counter > 0:
 		jump_buffer_counter -= 1
-	
-	if jump_buffer_counter > 0 and cayote_counter > 0: #Jump
+		
+	if Input.is_action_just_pressed("jump"): #Look if player jump
+		jump_buffer_counter = jump_buffer_time #Set buffer time
+		
+	if jump_buffer_counter > 0 and cayote_counter > 0: #Jump 
 		player_state = "jumping"
-		requested_animation = "jump"
+		idle_cooldown = 10.0 #Reset the player idle cooldown
+		if abs(velocity.x) == 100: #If player speed is high
+			animated_sprite.play("jump2") #Play a special jump animation
+		else:
+			animated_sprite.play("jump")
 		velocity.y = -jump_force
 		jump_buffer_counter = 0
 		cayote_counter = 0
-		
-		
-	animated_sprite.play(requested_animation)
 	move_and_slide()
+	
